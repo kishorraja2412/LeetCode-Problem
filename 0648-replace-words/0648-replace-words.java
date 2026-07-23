4cb7375e6035d64a2ddb8cd46f5c6c03367479ca
kishorraja2412 <kishorraja2412@gmail.com>
2026-07-23 15:05:53 +0530

    Time: 699 ms (16.43%), Space: 58.9 MB (84.27%) - LeetHub

---
+++ 0648-replace-words/0648-replace-words.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String replaceWords(List<String> dictionary, String sentence) {
+        Set <String> set=new HashSet<>(dictionary);
+        String arr[]=sentence.split(" ");
+        StringBuilder sb=new StringBuilder();
+        for(String i:arr){
+            String replace=i;
+            for(int j=1;j<=replace.length();j++){
+                String newword=replace.substring(0,j);
+                if(set.contains(newword)){
+                    replace = newword;
+                    break;
+                }
+            }sb.append(replace).append(" ");
+        } return sb.toString().trim();
+    }
+}
