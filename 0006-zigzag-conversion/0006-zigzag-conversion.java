db32fbbde990a9404e3981c801b95d8187b03295
kishorraja2412 <kishorraja2412@gmail.com>
2026-07-23 13:41:38 +0530

    Time: 6 ms (28.56%), Space: 46.8 MB (36.8%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String convert(String s, int numRows) {
+        if(numRows==1){
+            return s;
+        }
+        List<StringBuilder> m=new ArrayList<>();
+        for(int i=0;i<numRows;i++){
+            m.add(new StringBuilder());
+        }
+        int start=0;
+        boolean down=false;
+        for(char ch: s.toCharArray()){
+            m.get(start).append(ch);
+            if(start==0 || start==numRows-1){
+                down=!down;
+            }
+            start += down?1:-1;
+        }
+        StringBuilder sb=new StringBuilder();
+        for (StringBuilder row : m) {
+            sb.append(row);
+        }
+        return sb.toString();
+    }
+}
